932c1738d75feec46f49342c80af51eeee316b80
GreenSpaceDog <210127112+GreenSpaceDog@users.noreply.github.com>
2025-10-14 09:50:22 +0300

    playwright

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,6 +25,8 @@ export default [
       'object-curly-spacing': ['error', 'always'],
       'arrow-spacing': ['error', { before: true, after: true }],
       'no-console': 'off',
+      'react/prop-types': 0,
+      'react/react-in-jsx-scope': 'off'
     },
   },
   {
--- tests/pokedex.spec.js
+++ tests/pokedex.spec.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
@@ -5,5 +6,11 @@ describe('Pokedex', () => {
     await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('pokemon page can be navigated to', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
